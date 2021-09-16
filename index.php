e3dda74c20ac3cd95bfa77232695014685d49c37
coa989 <a.marjanovic989@gmail.com>
2021-09-16 14:25:17 +0200

    change output, implement polymorphism

--- index.php
+++ index.php
@@ -2,37 +2,36 @@
 
 use classes\Circle;
 use classes\Rectangle;
+use classes\Shape;
 use classes\Triangle;
 
 require_once 'autoload.php';
+
+function area(Shape $shape) {
+    return $shape->areaCalculator();
+}
+
+function circumference(Shape $shape) {
+    return $shape->circumferenceCalculator();
+}
 
 $rectangle = new Rectangle(10, 15);
 $circle = new Circle(5899);
 $triangle = new Triangle(89, 155, null);
 $triangle1 = new Triangle(89, 155, 151);
-?>
 
-<p>Rectangle Area:
-    <?= $rectangle->areaCalculator() ?>
-</p>
-<p>Rectangle Circumference:
-    <?= $rectangle->circumferenceCalculator() ?>
-</p>
-<p>Circle Area:
-    <?= $circle->areaCalculator() ?>
-</p>
-<p>Circle Circumference:
-    <?= $circle->circumferenceCalculator() ?>
-</p>
-<p>Triangle Area:
-    <?= $triangle->areaCalculator() ?>
-</p>
-<p>Triangle Circumference:
-    <?= $triangle->circumferenceCalculator() ?>
-</p>
-<p>Triangle 1 Area:
-    <?= $triangle1->areaCalculator() ?>
-</p>
-<p>Triangle 1 Circumference:
-    <?= $triangle1->circumferenceCalculator() ?>
-</p>
+echo area($rectangle);
+echo "\n";
+echo circumference($rectangle);
+echo "\n";
+echo area($circle);
+echo "\n";
+echo circumference($circle);
+echo "\n";
+echo area($triangle);
+echo "\n";
+echo circumference($triangle);
+echo "\n";
+echo area($triangle1);
+echo "\n";
+echo circumference($triangle1);
